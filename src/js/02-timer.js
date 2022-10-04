467ba1ab5e950764874d37c63ba37f43a2833de1
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-10-04 18:06:32 +0300

    final-corrected

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-refs = {
+const refs = {
   start: document.querySelector('[data-start]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
